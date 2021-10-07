68f4ed1f23f6d59c3630bcb7a626e3a521ebe516
Moch. Ari Nasichuddin <arynas92@gmail.com>
2021-10-07 21:39:35 +0700

    remove db config

--- sweeping.py
+++ sweeping.py
@@ -1,11 +1,7 @@
 from pymongo import MongoClient
 
-client = MongoClient('172.104.190.208', username='arynas',  password='Arynas92')
+client = MongoClient('YOURHOST', username='YOURUSERNAME',  password='YOURPASSWORD')
 db = client.sinta
 col_google_scholars = db.google_scholars
 
 print(list(col_google_scholars.find().sort('_id', -1).limit(1)))
-
-# ids = [data['_id'] for data in col_google_scholars.find().sort('_id', -1).limit(1273)]
-#
-# col_google_scholars.remove({'_id': {'$in': ids}})
